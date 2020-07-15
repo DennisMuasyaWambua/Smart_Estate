527232ec3a07b56536ff4030c4e0de33d51d847a
Dennis <muasyathegreat4@gmail.com>
2020-07-15 15:53:24 +0300

    new changes

--- app/src/main/java/com/example/smartestate/rentFragment.java
+++ app/src/main/java/com/example/smartestate/rentFragment.java
@@ -1,7 +1,12 @@
 package com.example.smartestate;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +15,11 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.NumberPicker;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,6 +28,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.Objects;
+
+import static android.app.Activity.RESULT_OK;
 
 public class rentFragment extends Fragment {
     private RecyclerView mRecyclerView;
@@ -33,6 +42,10 @@ public class rentFragment extends Fragment {
     private NumberPicker capacityPicker;
     private Button newBuilding, uploadPicture;
     private ImageButton takePicture;
+    private static final int IMAGE_PICK_CODE=1000;
+    private static final int PERMISSION_CODE=1001;
+    private static final int REQUEST_IMAGE_CAPTURE=1;
+
     ArrayList<recyclerItems>items = new ArrayList<>();
     @Nullable
     @Override
@@ -70,6 +83,8 @@ public class rentFragment extends Fragment {
         newBuilding.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String estate = nameEstate.getText().toString();
+                String building = nameBuilding.getText().toString();
 
 
 
@@ -82,8 +97,46 @@ public class rentFragment extends Fragment {
                 addBuildingDialog.dismiss();
             }
         });
+        takePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPermission();
+            }
+        });
 
         addBuildingDialog.show();
 
+
+    }
+
+    public void checkPermission(){
+        if(ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.CAMERA)!=PackageManager.PERMISSION_GRANTED){
+            String[] permission = {Manifest.permission.CAMERA};
+            requestPermissions(permission,PERMISSION_CODE);
+        }else{
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent,REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            checkPermission();
+        }else{
+            Toast.makeText(getContext(),"permission denied",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==REQUEST_IMAGE_CAPTURE&&resultCode==RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap photo = (Bitmap)extras.get("data");
+            imageOfEstate.setImageBitmap(photo);
+            imageOfEstate.setImageURI(data.getData());
+        }
     }
 }
